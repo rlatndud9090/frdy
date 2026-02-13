6b648f2cda1d420ce9c16a477fffe4625c1ddd9b
김수영 <43226931+rlatndud9090@users.noreply.github.com>
2026-02-13 13:56:08 +0900

    balance(map): 맵 깊이와 엣지 연결 제한 조정

--- data/map_configs/default_config.lua
+++ data/map_configs/default_config.lua
@@ -1,9 +1,10 @@
 return {
   floor_count = 3,
   nodes_per_column = {min = 2, max = 4},
-  columns_per_floor = {min = 5, max = 8},
+  -- 시작 노드 + 13개 중간 컬럼 + 보스 노드 ~= 깊이 15
+  columns_per_floor = {min = 14, max = 14},
   combat_ratio = 0.7,
   event_ratio = 0.3,
-  edges_per_node = {min = 1, max = 3},
+  edges_per_node = {min = 1, max = 2},
   segment_width = 300
 }
